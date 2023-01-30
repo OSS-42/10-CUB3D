8d067431429cd447ff042f0ead23e2cb50dc5795
xDeadpoolx <eric.wursteisen@gmail.com>
2023-01-30 10:30:30 -0500

    putting up initial checks of map

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:34:40 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/01/30 09:45:34 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/01/30 10:25:48 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,58 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
+
+typedef struct s_player
+{
+	void	*p_right;
+	void	*p_left;
+}	t_player;
+
+typedef struct s_level
+{
+	void	*corner_1;
+	void	*corner_2;
+	void	*corner_3;
+	void	*corner_4;
+	void	*wall_left;
+	void	*wall_right;
+	void	*wall_top;
+	void	*wall_bottom;
+	void	*floor;
+	void	*pilar;
+	void	*collect;
+	void	*start;
+	void	*exit;
+	int		x;
+	int		y;
+	int		img_x;
+	int		img_y;
+}	t_level;
+
+typedef struct s_vault
+{
+	void		*img;
+	void		*mlx;
+	void		*mlx_win;
+	char		**map;
+	char		*argv;
+	int			error_code;
+	int			char_check;
+	int			char_p;
+	int			char_e;
+	int			width;
+	int			height;
+	int			lines;
+	int			lenght;
+	int			moves;
+	int			collect;
+	int			total_c;
+	int			player_x;
+	int			player_y;
+	int			p_dir;
+	t_player	*player;
+	t_level		*lvl1;
+}	t_vault;
 
 #endif
--- src/cub3D.c
+++ src/cub3D.c
@@ -6,17 +6,55 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 09:33:50 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/01/30 09:45:59 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/01/30 10:30:00 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
+void	check_map_name(t_vault *data)
+{
+	char	*filename;
+	int		fd;
+
+	fd = open(data->argv, O_DIRECTORY);
+	if (fd > 0)
+	{
+		close(fd);
+		data->error_code = 12;
+		errors(data);
+	}
+	filename = ft_strrchr(data->argv, '.');
+	if (filename == NULL)
+	{
+		data->error_code = 8;
+		errors(data);
+	}
+	if (ft_strncmp(filename, ".cub", ft_strlen(filename)) != 0)
+	{
+		data->error_code = 8;
+		errors(data);
+	}
+}
+
+
 int	main(int argc, char **argv, char **env)
 {
-	(void)	argc;
-	(void)	argv;
-	(void)	env;
+	t_vault	data;
+
+	(void)argc;
+	(void)argv;
+	(void)env;
+	data.error_code = 0;
+	if (argc != 2)
+		data.error_code = 9;
+	// errors(&data);
+	data.argv = argv[1];
+	check_map_name(&data);
+	// map_to_array(&data);
+	// check_map(&data);
+	// errors(&data);
+	// newgame(&data);
 	printf("hello\n");
 	return (0);
 }
