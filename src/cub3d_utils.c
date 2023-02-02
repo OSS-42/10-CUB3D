cecaf24d2c904238b22c8355315f11399a90b770
xDeadpoolx <eric.wursteisen@gmail.com>
2023-02-02 14:54:45 -0500

    finish rgb parsing

--- src/cub3d_utils.c
+++ src/cub3d_utils.c
@@ -6,11 +6,29 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 23:21:15 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/02/02 13:51:50 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/02/02 14:52:48 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
+
+int	ft_find_char(char *src, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (!src || c == 0)
+		return (count);
+	while (src[i])
+	{
+		if (src[i] == c)
+			count++;
+		i++;
+	}
+	return (count);
+}
 
 // on ne gere pas si le code rgb a plus que ou moins que 3 parametres
 int	correct_rgb_range(t_vault *data, char *rgb_code)
@@ -18,7 +36,6 @@ int	correct_rgb_range(t_vault *data, char *rgb_code)
 	int		code_r;
 	int		code_g;
 	int		code_b;
-	int		comma_count;
 	int		i;
 	int		x;
 	int		len;
@@ -27,17 +44,12 @@ int	correct_rgb_range(t_vault *data, char *rgb_code)
 	code_r = -1;
 	code_g = -1;
 	code_b = -1;
+	i = 0;
 	x = 0;
 	len = 0;
 	temp = NULL;
 
-	while (rgb_code && rgb_code[i])
-	{
-		if (ft_strchr(rgb_code, ',') == 1)
-			comma_count++;
-		i++;
-	}
-	if (comma_count != 2)
+	if (ft_find_char(rgb_code, ',') != 2)
 	{
 		data->error_code = 15;
 		errors(data);
@@ -48,7 +60,7 @@ int	correct_rgb_range(t_vault *data, char *rgb_code)
 			x++;
 	}
 	temp = ft_substr(rgb_code, len, x - len);
-	if (temp)
+	if (temp[0] != '\0')
 		code_r = ft_atoi(temp);
 	free (temp);
 	x++;
@@ -59,7 +71,7 @@ int	correct_rgb_range(t_vault *data, char *rgb_code)
 			x++;
 	}
 	temp = ft_substr(rgb_code, len, x - len);
-	if (temp)
+	if (temp[0] != '\0')
 		code_g = ft_atoi(temp);
 	free (temp);
 	x++;
@@ -70,7 +82,7 @@ int	correct_rgb_range(t_vault *data, char *rgb_code)
 			x++;
 	}
 	temp = ft_substr(rgb_code, len, x - len);
-	if (temp)
+	if (temp[0] != '\0')
 		code_b = ft_atoi(temp);
 	free (temp);
 	temp = NULL;
