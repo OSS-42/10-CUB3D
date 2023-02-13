b4a3923f21215df167023c265dfc56c303013dd3
xDeadpoolx <eric.wursteisen@gmail.com>
2023-02-13 11:53:27 -0500

    update dessiner la canne et orientation

--- src/camera.c
+++ src/camera.c
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 22:40:10 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/02/11 22:40:55 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:53:08 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,19 @@ void	draw_pov(t_vault *data)
 	float	y;
 	int		len;
 
-	len = 50;
-	x = data->player->px * 11 + 4;
-	y = data->player->py * 11 + 4;
-	while (len > 0)
+	len = 0;
+	x = data->player->ppx;
+	y = data->player->ppy;
+	data->player->pdlen = 10;
+	while (len < data->player->pdlen)
 	{
-		mlx_put_pixel(data->minimap->minimap, y, x, 0xFF00FFFF);
-		x += data->player->pdx;
-		y += data->player->pdy;
-		len--;
+		printf("ppx= %.3f ppy= %.3f pa= %.3f x= %.3f y= %.3f\n",
+		data->player->ppx, data->player->ppy, data->player->pa,
+		x, y);
+		mlx_put_pixel(data->minimap->minimap, x, y, 0xFF00FFFF);
+		x = x + data->player->pdx;
+		y = y + data->player->pdy;
+		len++;
 	}
 }
 
@@ -36,8 +40,8 @@ void	rotate_left(t_vault *data)
 	data->player->pa = data->player->pa - 0.1;
 	if (data->player->pa < 0)
 		data->player->pa = data->player->pa + 2 * PI;
-	data->player->pdx = sin(data->player->pa) * 0.3;
-	data->player->pdy = cos(data->player->pa) * 0.3;
+	data->player->pdx = cos(data->player->pa);
+	data->player->pdy = sin(data->player->pa);
 	draw_player(data);
 }
 
@@ -47,7 +51,7 @@ void	rotate_right(t_vault *data)
 	data->player->pa = data->player->pa + 0.1;
 	if (data->player->pa > 2 * PI)
 		data->player->pa = data->player->pa - 2 * PI;
-	data->player->pdx = sin(data->player->pa) * 0.3;
-	data->player->pdy = cos(data->player->pa) * 0.3;
+	data->player->pdx = cos(data->player->pa);
+	data->player->pdy = sin(data->player->pa);
 	draw_player(data);
 }
--- src/init_player.c
+++ src/init_player.c
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:16:45 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/02/11 23:14:50 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:46:57 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 void	load_player(t_vault *data)
 {
 	init_player(data);
-	find_orientation(data,
-		data->map->map[data->player->start_x][data->player->start_y]);
+	find_orientation(data, data->player->direction);
 	data->map->map[data->player->start_x][data->player->start_y] = '0';
 	draw_player(data);
 }
@@ -25,51 +24,54 @@ void	init_player(t_vault *data)
 {
 	data->player->px = data->player->start_x;
 	data->player->py = data->player->start_y;
+	data->player->ppx = data->player->px * 11 + 4;
+	data->player->ppy = data->player->py * 11 + 4;
 }
 
 void	find_orientation(t_vault *data, char direction)
 {
+
 	if (direction == 'N')
 	{
-		data->player->pa = 3 * PI / 2;
-		data->player->pdx = sin(data->player->pa) * 0.3;
-		data->player->pdy = 0;
+		data->player->pa = 90;
+		data->player->pdx = 0;
+		data->player->pdy = -1;
 	}
 	else if (direction == 'S')
 	{
-		data->player->pa = PI / 2;
-		data->player->pdx = sin(data->player->pa) * 0.3;
-		data->player->pdy = 0;
+		data->player->pa = 270;
+		data->player->pdx = 0;
+		data->player->pdy = 1;
 	}
 	else if (direction == 'E')
 	{
 		data->player->pa = 0;
-		data->player->pdx = 0;
-		data->player->pdy = cos(data->player->pa) * 0.3;
+		data->player->pdx = 1;
+		data->player->pdy = 0;
 	}
 	else if (direction == 'W')
 	{
-		data->player->pa = PI;
-		data->player->pdx = 0;
-		data->player->pdy = cos(data->player->pa) * 0.3;
+		data->player->pa = 170;
+		data->player->pdx = -1;
+		data->player->pdy = 0;
 	}
 }
 
 void	draw_player(t_vault *data)
 {
-	printf("px= %.3f py= %.3f pa= %.3f pdx= %.3f pdy= %.3f\n",
-		data->player->px, data->player->py, data->player->pa,
-		data->player->pdx, data->player->pdy);
+	// printf("ppx= %.3f ppy= %.3f pa= %.3f pdx= %.3f pdy= %.3f\n",
+	// 	data->player->ppx, data->player->ppy, data->player->pa,
+	// 	data->player->pdx, data->player->pdy);
 	mlx_put_pixel(data->minimap->minimap,
-		data->player->py * 11 + 4, data->player->px * 11 + 4, 0x00FF00FF);
+		data->player->ppx, data->player->ppy, 0x00FF00FF);
 	mlx_put_pixel(data->minimap->minimap,
-		data->player->py * 11 + 4, data->player->px * 11 + 4 + 1, 0x00FF00FF);
+		data->player->ppx, data->player->ppy + 1, 0x00FF00FF);
 	mlx_put_pixel(data->minimap->minimap,
-		data->player->py * 11 + 4, data->player->px * 11 + 4 - 1, 0x00FF00FF);
+		data->player->ppx, data->player->ppy - 1, 0x00FF00FF);
 	mlx_put_pixel(data->minimap->minimap,
-		data->player->py * 11 + 4 + 1, data->player->px * 11 + 4, 0x00FF00FF);
+		data->player->ppx + 1, data->player->ppy, 0x00FF00FF);
 	mlx_put_pixel(data->minimap->minimap,
-		data->player->py * 11 + 4 - 1, data->player->px * 11 + 4, 0x00FF00FF);
+		data->player->ppx - 1, data->player->ppy, 0x00FF00FF);
 	// mlx_put_pixel(data->minimap->minimap,
 	// 	data->player->py * 11 + 4 + 1, data->player->px * 11 + 4 + 1, 0x00FF00FF);
 	// mlx_put_pixel(data->minimap->minimap,
@@ -81,4 +83,3 @@ void	draw_player(t_vault *data)
 	draw_pov(data);
 	// draw_rays(data);
 }
-
