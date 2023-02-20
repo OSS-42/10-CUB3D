84d1546e0ff2dc5723a42ef97ec9928c7a704551
Morgan <bertin.morgan@hotmail.fr>
2023-02-20 13:41:50 -0500

    Double save

--- raycaster_flat.cpp
+++ raycaster_flat.cpp
@@ -73,7 +73,7 @@ int main(int /*argc*/, char */*argv*/[])
 {
   double posX = 22, posY = 12;  //x and y start position
   double dirX = -1, dirY = 0; //initial direction vector
-  double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+  double p //the 2d raycaster version of camera plane
 
   double time = 0; //time of current frame
   double oldTime = 0; //time of previous frame
--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: mbertin <mbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 23:54:21 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/02/20 10:56:21 by mbertin          ###   ########.fr       */
+/*   Updated: 2023/02/20 13:36:07 by mbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ void	raycaster(t_vault *data)
 	side = 0;
 	col = data->player->px;
 	row = data->player->py;
+	delta_dist_x = 0;
+	delta_dist_y = 0;
 	data->raycaster->ray_one_a = data->player->pa - degtorad(32);
 //	while(data->raycaster->ray_count < 64)
 //	{
@@ -44,8 +46,12 @@ void	raycaster(t_vault *data)
 		printf("ray_one_a: %f°\n", data->raycaster->ray_one_a * 57.29578);
 		ray_len_x = ray_seg_len_x(data, data->player->ppx, 'P');
 		ray_len_y = ray_seg_len_y(data, data->player->ppy, 'P');
-		opp_side_x = ray_len_x * sin(data->raycaster->ray_one_a);
-		opp_side_y = ray_len_y * sin(degtorad(90) - data->raycaster->ray_one_a);
+		printf("longueur du rayon x la premiere fois : %f\n", ray_len_x);
+		printf("longueur du rayon y la premiere fois : %f\n", ray_len_y);
+		opp_side_x = ray_len_x * sin(degtorad(90) - data->raycaster->ray_one_a);
+		opp_side_y = ray_len_y * sin(data->raycaster->ray_one_a);
+		printf("ppx : %d\n", data->player->ppx);
+		printf("ppy : %d\n", data->player->ppy);
 		if (data->raycaster->pdx_ray < 0)
 		{
 			map_2d_col = -1;
@@ -59,13 +65,15 @@ void	raycaster(t_vault *data)
 		if (data->raycaster->pdy_ray < 0)
 		{
 			map_2d_row = -1;
-			delta_dist_y = ray_seg_len_x(data, data->player->ppy - opp_side_y, 'R');
+			delta_dist_y = ray_seg_len_y(data, data->player->ppy - opp_side_y, 'R');
 		}
 		else if (data->raycaster->pdy_ray > 0)
 		{
 			map_2d_row = 1;
-			delta_dist_y = ray_seg_len_x(data, data->player->ppy + opp_side_y, 'R');
+			delta_dist_y = ray_seg_len_y(data, data->player->ppy + opp_side_y, 'R');
 		}
+		printf("longueur de delta x la premiere fois : %f\n", delta_dist_x);
+		printf("longueur de delta y la premiere fois : %f\n", delta_dist_y);
 		printf("\ncoordonnees depart:\nppx =	%d\nppy =	%d\n", data->player->ppx, data->player->ppy);
 		printf("\ncoordonnees depart:\npx =	%d\npy =	%d\n", data->raycaster->next_x, data->raycaster->next_y);
 		while (wall_in_next_case(data, row, col) == FALSE)
@@ -78,6 +86,7 @@ void	raycaster(t_vault *data)
 				printf("\033[1;93m");
 				printf("\nINTERCEPTION EN X -> RAY_LEN_X est le plus court\n");
 				printf("\033[1;0m");
+				printf("ray_len_x dans la boucle = %f\n", ray_len_x);
 				ray_len_x += delta_dist_x;
 				col += map_2d_col;
 				side = 0;
@@ -88,12 +97,14 @@ void	raycaster(t_vault *data)
 				printf("\033[1;93m");
 				printf("\nINTERCEPTION EN Y -> RAY_LEN_Y est le plus court\n");
 				printf("\033[1;0m");
+				printf("ray_len_y dans la boucle = %f\n", ray_len_y);
 				ray_len_y += delta_dist_y;
 				row += map_2d_row;
 				side = 1;
 				ray_len = ray_len_y;
 			}
 		}
+		printf("Valeur final de ray_len : %f\n", ray_len);
 		draw_ray(data, ray_len);
 //		data->raycaster->ray_one_a = data->raycaster->ray_one_a + degtorad(1);
 //		data->raycaster->ray_count++;
@@ -127,4 +138,163 @@ void	find_ray_angle(t_vault *data)
 		data->raycaster->ray_one_a = data->raycaster->ray_one_a - 2 * PI;
 	data->raycaster->pdx_ray = cos(data->raycaster->ray_one_a);
 	data->raycaster->pdy_ray = sin(data->raycaster->ray_one_a);
+}
+
+
+void	raycaster(t_vault *data)
+{
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	ray_len_x;
+	double	ray_len_y;
+	double	ray_len;
+	int		opp_side_x;
+	int		opp_side_y;
+	int		map_2d_col;
+	int		map_2d_row;
+	int		col;
+	int		row;
+	int		side;
+
+	ray_len = 0;
+	side = 0;
+	col = data->player->px;
+	row = data->player->py;
+	delta_dist_x = 0;
+	delta_dist_y = 0;
+	data->raycaster->ray_one_a = data->player->pa - degtorad(32);
+//	while(data->raycaster->ray_count < 64)
+//	{
+		data->raycaster->next_x = data->player->ppy / 65;
+		data->raycaster->next_y = data->player->ppx / 65;
+		find_ray_angle(data);
+		ray_len_x = ray_seg_len_x(data, data->player->ppx, 'P');
+		ray_len_y = ray_seg_len_y(data, data->player->ppy, 'P');
+		opp_side_x = ray_len_x * sin(degtorad(90) - data->raycaster->ray_one_a);
+		opp_side_y = ray_len_y * sin(data->raycaster->ray_one_a);
+		if (data->raycaster->pdx_ray < 0)
+		{
+			map_2d_col = -1;
+			delta_dist_x = ray_seg_len_x(data, data->player->ppx - opp_side_x, 'R');
+		}
+		else if (data->raycaster->pdx_ray > 0)
+		{
+			map_2d_col = 1;
+			delta_dist_x = ray_seg_len_x(data, data->player->ppx + opp_side_x, 'R');
+		}
+		if (data->raycaster->pdy_ray < 0)
+		{
+			map_2d_row = -1;
+			delta_dist_y = ray_seg_len_y(data, data->player->ppy - opp_side_y, 'R');
+		}
+		else if (data->raycaster->pdy_ray > 0)
+		{
+			map_2d_row = 1;
+			delta_dist_y = ray_seg_len_y(data, data->player->ppy + opp_side_y, 'R');
+		}
+		while (wall_in_next_case(data, row, col) == FALSE)
+		{
+			if (ray_len_x < ray_len_y)
+			{
+				ray_len_x += delta_dist_x;
+				col += map_2d_col;
+				side = 0;
+				ray_len = ray_len_x;
+			}
+			else
+			{
+				ray_len_y += delta_dist_y;
+				row += map_2d_row;
+				side = 1;
+				ray_len = ray_len_y;
+			}
+		}
+		draw_ray(data, ray_len);
+//		data->raycaster->ray_one_a = data->raycaster->ray_one_a + degtorad(1);
+//		data->raycaster->ray_count++;
+//	}
+	data->raycaster->ray_count = 0;
+}
+
+
+void	raycaster(t_vault *data)
+{
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	ray_len_x;
+	double	ray_len_y;
+	double	ray_len;
+	// int		opp_side_x;
+	// int		opp_side_y;
+	int		map_2d_col;
+	int		map_2d_row;
+	int		col;
+	int		row;
+	int		side;
+	int		pp_seg_y;
+	int		pp_seg_x;
+	int		adj_x;
+	int		adj_y;
+
+	ray_len = 0;
+	side = 0;
+	col = data->player->px;
+	row = data->player->py;
+	delta_dist_x = 0;
+	delta_dist_y = 0;
+	data->raycaster->ray_one_a = data->player->pa - degtorad(32);
+	pp_seg_x = data->player->ppx % 65;
+	pp_seg_y = data->player->ppy % 65;
+//	while(data->raycaster->ray_count < 64)
+//	{
+		data->raycaster->next_x = data->player->ppy / 65;
+		data->raycaster->next_y = data->player->ppx / 65;
+		find_ray_angle(data);
+		ray_len_x = ray_seg_len_x(data, data->player->ppx, 'P');
+		ray_len_y = ray_seg_len_y(data, data->player->ppy, 'P');
+		// opp_side_x = ray_len_x * sin(degtorad(90) - data->raycaster->ray_one_a);
+		// opp_side_y = ray_len_y * sin(data->raycaster->ray_one_a);
+		if (data->raycaster->pdx_ray < 0)
+		{
+			map_2d_col = -1;
+
+			delta_dist_x = ray_seg_len_x(data, data->player->ppx - opp_side_x, 'R');
+		}
+		else if (data->raycaster->pdx_ray > 0)
+		{
+			map_2d_col = 1;
+			delta_dist_x = ray_seg_len_x(data, data->player->ppx + opp_side_x, 'R');
+		}
+		if (data->raycaster->pdy_ray < 0)
+		{
+			map_2d_row = -1;
+			delta_dist_y = ray_seg_len_y(data, data->player->ppy - opp_side_y, 'R');
+		}
+		else if (data->raycaster->pdy_ray > 0)
+		{
+			map_2d_row = 1;
+			delta_dist_y = ray_seg_len_y(data, data->player->ppy + opp_side_y, 'R');
+		}
+		while (wall_in_next_case(data, row, col) == FALSE)
+		{
+			if (ray_len_x < ray_len_y)
+			{
+				ray_len_x += delta_dist_x;
+				col += map_2d_col;
+				side = 0;
+				ray_len = ray_len_x;
+			}
+			else
+			{
+				ray_len_y += delta_dist_y;
+				row += map_2d_row;
+				side = 1;
+				ray_len = ray_len_y;
+			}
+		}
+		draw_ray(data, ray_len);
+//		data->raycaster->ray_one_a = data->raycaster->ray_one_a + degtorad(1);
+//		data->raycaster->ray_count++;
+//	}
+	data->raycaster->ray_count = 0;
 }
