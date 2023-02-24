4804dccc30f308dc9cd8d25169c88c07fb6f62e8
Morgan <bertin.morgan@hotmail.fr>
2023-02-24 10:09:15 -0500

    Clean de init_3d_bonus

--- bonus/src/init_3d_bonus.c
+++ bonus/src/init_3d_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_3d_bonus.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
+/*   By: mbertin <mbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 22:12:30 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/02/23 23:42:25 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/02/24 09:10:35 by mbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,6 @@ void	load_3d(t_vault *data)
 			data->raycaster->height_3d);
 	mlx_image_to_window(data->mlx, data->game->ddd, 0, 0);
 	background_3d(data);
-}
-
-void	background_3d(t_vault *data)
-{
-	int	i;
-	int	start;
-
-	i = 0;
-	while (i < data->raycaster->height_3d / 2)
-	{
-		start = -1;
-		while (++start < WIDTH)
-			mlx_put_pixel(data->game->ddd, start,
-				i, data->scene_param->hex_ceiling);
-		i++;
-	}
-	while (i < data->raycaster->height_3d)
-	{
-		start = -1;
-		while (++start < WIDTH)
-			mlx_put_pixel(data->game->ddd, start,
-				i, data->scene_param->hex_floor);
-		i++;
-	}
-}
-
-void	reinit_3d(t_vault *data)
-{
-	mlx_delete_image(data->mlx, data->game->ddd);
-	load_3d(data);
 }
 
 void	background_3d(t_vault *data)
